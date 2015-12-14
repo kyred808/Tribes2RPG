f93530c9d4a5743239fd6e7629872170e174208e
kyred808 <kyred808@gmail.com>
2015-12-13 19:51:15 -0600

    Initial file commit

---
+++ scripts/rpgmana.cs
@@ -0,0 +1,53 @@
+function setMANA(%client, %val)
+{
+	%armor = %client.player.getDataBlock();
+
+	if(%val $= "")
+		%val = fetchData(%client, "MaxMANA");
+
+	%a = %val * %armor.maxEnergy;
+	%b = %a / fetchData(%client, "MaxMANA");
+
+	if(%b < 0)
+		%b = 0;
+	else if(%b > %armor.maxEnergy)
+		%b = %armor.maxEnergy;
+
+	%client.player.setEnergyLevel(%b);
+}
+function refreshMANA(%client, %value)
+{
+	setMANA(%client, (fetchData(%client, "MANA") - %value));
+}
+
+// Positively increase mana and not worry about overrunning the maximum allowed.
+function restoreMANA(%client, %value)
+{
+    %maxMana = fetchData(%client, "MaxMANA");
+    %currentMana = fetchData(%client, "MANA");
+    if (%value + %currentMana > %maxMana)
+        setMANA(%client, %maxMana);
+    else
+        setMANA(%client, %currentMana + %value);
+}
+
+function refreshMANAREGEN(%client)
+{
+	if(isobject(%client) && isobject(%client.player))
+	{
+		%b = 0;
+
+		if(%client.sleepMode $= 1)
+			%b = 1.0;
+		else if(%client.sleepMode $= 2)
+			%b = 2.25;
+
+		%c = AddPoints(%client, 11) / 10000;
+		%r = %b/20 + %c + 0.02;
+
+		%client.player.setRechargeRate(%r);
+		return true;
+	}
+	else
+		return false;
+}
